3e5c1a5f11318abd518be353d7dc09762af7c8c8
Tai Nguyen Trong <taint@vmodev.com>
2022-01-07 16:18:07 +0700

    Init: Setup React Query, Emotion Cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MainLayout } from '@/layouts/index';
+import { queryClient } from '@/queries/index';
+import { createEmotionCache, theme } from '@/styles/index';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import * as React from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type CustomNextPage = NextPage & {
+  Layout?: React.FC;
+};
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+  Component: CustomNextPage;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
+  const Layout = Component.Layout || MainLayout;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </CacheProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
